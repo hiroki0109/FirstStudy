caf7b6ade0b09b815b15d52ad8f6518885f3c940
hiroki0109 <hiroki1090000315@gmail.com>
2022-08-15 22:09:26 +0900

    背景色を黄色に変更

---
+++ android/app/src/main/kotlin/com/example/kiso/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kiso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:kiso/next_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  List<String> titleList = [
+    'amazon',
+    '楽天',
+    'yahoo!',
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('パスワード管理'),
+      ),
+      body: ListView.builder(
+        itemCount: titleList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.vpn_key),
+                title: Text(titleList[index]),
+                onTap: () {
+                  ///画面遷移の記述
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => NextPage()));
+                },
+              ),
+              Divider(),
+            ],
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          titleList.add('google');
+          setState(() {});
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
